277b6c0a4078d6323d335ceba23509cc2a4339e3
Tommy882646 <92374601+Tommy882646@users.noreply.github.com>
2022-10-12 08:21:01 -0400

    Update FoodReview.java

--- java-classes/FoodReview.java
+++ java-classes/FoodReview.java
@@ -1,7 +1,7 @@
 import java.time.LocalDate;
 
 /**
-  describe this class
+  this class allows a user to add or modify reviews for a restaurant.
 */
 public class FoodReview
 {
@@ -23,18 +23,92 @@ public class FoodReview
     
     /**
       describe this constructor
-      @param
-      @param
-      @param
-      @param
-      @param
+      @param _restaurant for the specific restaurant the user desires
+      @param _reviewScore for the review score the user associates with the restaurant
+      @param _reviewTitle for the title the user associates with the restaurant
+      @param _reviewBody for the body the user associates with the restaurant 
+      @param _user for the user thats rating the restaurant
     */
     public FoodReview(Restaurant _restaurant, double _reviewScore, String _reviewTitle, String _reviewBody, User _user)
     {
+      restaurant = _restaurant;
+      reviewScore = _reviewScore;
+      reviewTitle = _reviewTitle;
+      reviewBody = _reviewBody;
+      user = _user;
+        
         // set instance var values using parameters
         // TO DO
     }
 
-    // accessors and mutators go here
+    public Restaurant getRestaurant(){
+      return restaurant;
+    }
+    
+    public void setRestaurant(String _retaurant){
+      restaurant = resaurant;
+    }
+  
+    public double getReviewScore(){
+      return reviewScore;
+    }
+    
+    public void setReviewScore(double _reviewScore){
+      reviewScore = _reviewScore;
+    }
+  
+    public String getReviewTitle(){
+      return reviewTitle;
+    }
+  
+    public void setReviewTitle(String _reviewTitle){
+      reviewTitle = _reviewTitle;
+    }
+  
+    public String getReviewBody(){
+      return reviewBody;
+    }
+  
+    public void setReviewBody(String _reviewBody){
+      reviewBody = _reviewBody;
+    }
+  
+    public User getUser(){
+      return user;
+    }
+  
+    public void setUser(User _user){
+      user = _user;
+    }
+
+  
+  
+  
+  
+   
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
 }
